bb83b614e38a9d9f572a277f84797a39a39d783c
Isak Svarre <Isak.svarre@gmail.com>
2025-02-14 12:12:16 +0100

    Getting Started

---
+++ nuxt.config.ts
@@ -0,0 +1,22 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2024-11-01",
+  devtools: { enabled: false },
+  modules: [
+    '@nuxtjs/tailwindcss'
+  ],
+  tailwindcss: {
+    config: {
+      content: [
+        "./components/**/*.{js,vue,ts}",
+        "./layouts/**/*.vue",
+        "./pages/**/*.vue",
+        "./plugins/**/*.{js,ts}",
+        "./nuxt.config.{js,ts}"
+      ]
+    }
+  },
+  css: [
+    '~/assets/css/main.scss'
+  ],
+});
