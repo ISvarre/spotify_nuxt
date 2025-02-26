6a38fe348fe1d1f6f14d093f953a9a4ab399fea3
Isak Svarre <Isak.svarre@gmail.com>
2025-02-26 10:58:07 +0100

    Home Page styling and Package updates

--- app.vue
+++ app.vue
@@ -15,18 +15,19 @@ async function fetchArtists() {
 </script>
 
 <template>
-  <div>
-    <form @submit.prevent="fetchArtists">
-      <input
-          v-model="artistName"
-          type="text"
-          @keyup.enter="fetchArtists"
-          placeholder="Artist"
-          class="px-2 p-1 border-2 border-solid border-black rounded-xl"
-      />
-    </form>
+  <div class="flex flex-col justify-center items-center gap-4 py-4">
+    <div class="flex flex-col gap-2 bg-white p-4 rounded-lg shadow-lg">
+      <form @submit.prevent="fetchArtists">
+        <input
+            v-model="artistName"
+            type="text"
+            placeholder="Artist"
+            class="px-2 p-1 border-2 border-solid border-black rounded-xl"
+        />
+      </form>
+    </div>
     <div v-if="artistListData" class="bg-white p-4 rounded-lg shadow-lg h-full">
-      <div  class="grid grid-cols-4 gap-4 overflow-y-auto w-full px-2 pb-15">
+      <div  class="grid grid-cols-3 gap-4 overflow-y-auto w-full px-2 pb-15">
         <ArtistCard
             v-for="artist in artistListData"
             :key="artist.name"
